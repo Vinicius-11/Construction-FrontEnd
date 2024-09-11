cb64e3925a8179b9522a3313b20e945b97fd47d5
Vinicius <viniciusdub8@gmail.com>
2024-09-10 21:55:39 -0300

    Adicionando arquivos da aula05

---
+++ praticas/praticas05/src/components/InputEmail.jsx
@@ -0,0 +1,10 @@
+function InputEmail () {
+    return (
+        <>
+            <label htmlFor="">E-mail</label>
+            <input type="email" id="email" name="email" required />
+        </>
+    );
+}
+
+export default InputEmail;
---
+++ praticas/praticas05/src/components/Menu.jsx
@@ -0,0 +1,9 @@
+function Menu () {
+    const opcoes = ["Home", "Perfil", "Sair"]
+
+    return (
+        <nav><ul></ul></nav>
+    );
+}
+
+export default Menu;
