8cc993938ecd0c4b71b34bedcc7fd07464ac9e0a
Vinicius <viniciusdub8@gmail.com>
2024-11-01 21:02:07 -0300

    Adicionando arquivos da praticas10

---
+++ praticas/praticas10/front/src/components/Cabecalho.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Menu from "../components/Menu";
+
+function Cabecalho() {
+    return (
+            <header>
+                <h1>Agenda</h1>
+                <Menu />
+            </header>
+    );
+}
+
+export default Cabecalho;
